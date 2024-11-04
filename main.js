396a94b8b284f6cb4903f3d6aea5d11712ce1f81
FreddyLin <144164104+FreddyLin@users.noreply.github.com>
2024-11-04 10:53:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -601,7 +601,7 @@ function showLocationDetails(location) {
     setupCarousel();
 
     // Initialize map
-    const map = L.map('map').setView(location.geo, 14);
+    const map = L.map('map').setView(location.geo, 15);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
